44c447c89e1eda6596481215340f0a186d5890d9
ekdnlt <ekdnlt1532@naver.com>
2021-04-15 14:44:15 +0900

    savePower

--- app/src/main/java/com/example/getoreintation/MainActivity.java
+++ app/src/main/java/com/example/getoreintation/MainActivity.java
@@ -77,4 +77,18 @@ public class MainActivity extends AppCompatActivity {
     private float radian2Degree(float radian) {
         return radian * 180 / (float) Math.PI;
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listener, magSensor,SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(listener, accSensor,SensorManager.SENSOR_DELAY_UI);
+
+    }
 }
